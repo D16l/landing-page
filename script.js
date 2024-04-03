62a10c05a70da828e081f9b8b04c84eb9be188a3
Denny Sulyvan Peyerl <contato@devsspace.com.br>
2024-04-02 23:06:30 -0300

    Adicionado forms e modificado sections

--- script.js
+++ script.js
@@ -27,3 +27,36 @@ function fecharMenu(elementoIcone) {
     const menu = elementoIcone.closest('.menu-botoes');
     menu.classList.remove('visible');
 }
+
+document.getElementById('userPhone').addEventListener('input', function(e) {
+    var phone = e.target.value.replace(/\D/g, '');
+
+    if(phone.lengh <= 2){
+        e.target.value = '(' + phone;
+    } else if (phone.lengh <= 6){
+        e.target.value = '(' + phone.substring(0,2) + ') ' + phone.substring(2);
+    } else if (phone.lengh <= 10) {
+        e.target.value = '(' + phone.substring(0,2) + ') ' + phone.substring(2,7) + '-' + phone.substring(7);
+    } else {
+        e.target.value = '(' + phone.substring(0,2) + ') ' + phone.substring(2,7) + '-' + phone.substring(7,11);
+    }
+});
+
+document.getElementById('userPhone').addEventListener('keydown', function (e) {
+    if (e.keyCode === 8) {
+        var phone = e.target.value.replace(/\D/g, '');
+        var formattedPhone = '';
+
+        if (phone.length <= 2) {
+            formattedPhone = phone;
+        } else if (phone.length <= 7) {
+            formattedPhone = '(' + phone.substring(0, 2) + ') ' + phone.substring(2);
+        } else if (phone.length <= 11) {
+            formattedPhone = '(' + phone.substring(0, 2) + ') ' + phone.substring(2, 7) + '-' + phone.substring(7);
+        } else {
+            formattedPhone = '(' + phone.substring(0, 2) + ') ' + phone.substring(2, 7) + '-' + phone.substring(7, 11);
+        }
+
+        e.target.value = formattedPhone;
+    }
+});
